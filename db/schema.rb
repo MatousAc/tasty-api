8c7893c3690a12c3e45ceb06825f480b3ee3bd26
MatousAc <matoush@southern.edu>
2020-12-13 11:11:52 -0500

    seeding db and moving notcode

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_213534) do
+ActiveRecord::Schema.define(version: 2020_12_13_143504) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "imageURL"
   end
 
   create_table "recipe_categories", force: :cascade do |t|
